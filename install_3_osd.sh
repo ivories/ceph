6b00bbbffe9a86acb117304870bee2bc354eb6af
Your Name <you@example.com>
2023-07-11 09:56:15 +0000

    up

--- install_3_osd.sh
+++ install_3_osd.sh
@@ -16,6 +16,14 @@ for i in $(cat hosts.txt); do
         ssh ${i} "systemctl restart ceph-osd@${OSDID};systemctl enable ceph-osd@${OSDID}"
         OSDID=$((OSDID+1))
     done
+    DISKS=$(ssh ${i} "lsblk -dp | grep -v /boot | grep -o '^/dev/sd[^ ]*' | grep -v /dev/sda")
+    for DISK in $DISKS; do
+        ssh ${i} "sudo -u ceph mkdir -p /var/lib/ceph/osd/ceph-${OSDID}"
+        ssh ${i} "ceph-volume lvm create --data ${DISK}"
+        ssh ${i} "systemctl restart ceph-osd@${OSDID};systemctl enable ceph-osd@${OSDID}"
+        OSDID=$((OSDID+1))
+    done
+
 done
 
 echo $OSDID > osdid.txt
